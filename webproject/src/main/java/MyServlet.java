0b5affd06f3203934844e432845a0de3adda98d5
Minwook Chae <mcchae@unomaha.edu>
2021-09-29 20:59:53 -0500

    rescan check gui

--- webproject/src/main/java/MyServlet.java
+++ webproject/src/main/java/MyServlet.java
@@ -31,7 +31,7 @@ public class MyServlet extends HttpServlet {
 		response.getWriter().append("Hello World!"); 
 		response.getWriter().println("<h1>Hello World 2!</h1>"); 
 		String x = "<p>Hello World 3!</p>"; 
-		// git check
+		// git check rescan 
 		response.getWriter().println(x); 
 	}
 
